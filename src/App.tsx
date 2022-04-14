9c5b471d06cbd6cfa2bc37b627632b104a1f16f1
sangsang6914 <sangsang6914@bandisnc.com>
2022-04-14 15:34:50 +0900

    jandi github connect test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import { useTranslation } from "react-i18next";
 
 function App() {
   const { t } = useTranslation();
-  return <div>{t("main.message")}</div>;
+  return (
+    <>
+      <div>{t("main.message")}</div>;<div>git hub test</div>
+      <div>Jandi Connect Test</div>
+    </>
+  );
 }
 
 export default App;
